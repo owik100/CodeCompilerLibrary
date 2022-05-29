a24fd08e15ab5182d321da472849539310448676
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2022-05-29 14:08:44 +0200

    Assembly ref nuget pack, compile to memory

--- CodeCompiler/CodeCompiler.cs
+++ CodeCompiler/CodeCompiler.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using Basic.Reference.Assemblies;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Emit;
 using Microsoft.CodeAnalysis.Text;
+using System.Reflection;
 using System.Text;
 
 namespace CodeCompilerNs
@@ -10,14 +12,23 @@ namespace CodeCompilerNs
     {
         public CodeCompiler()
         {
-            PrepareDefaultReferences("");
+
         }
 
-        public CodeCompiler(string runtimePath = "", IEnumerable<string> defaultNamespaces = null, CSharpCompilationOptions cSharpCompilationOptions = null)
+        public CodeCompiler(IEnumerable<PortableExecutableReference> referenceAssemblies)
         {
-            DefaultNamespaces = defaultNamespaces == null ? DefaultNamespaces : defaultNamespaces;
-            DefaultCompilationOptions = cSharpCompilationOptions == null ? DefaultCompilationOptions : cSharpCompilationOptions;
-            PrepareDefaultReferences(runtimePath);
+            ReferenceAssembliesProp = referenceAssemblies;
+        }
+
+        public CodeCompiler(CSharpCompilationOptions cSharpCompilationOptions)
+        {
+            CompOptions = cSharpCompilationOptions;
+        }
+
+        public CodeCompiler(IEnumerable<PortableExecutableReference> referenceAssemblies, CSharpCompilationOptions cSharpCompilationOptions)
+        {
+            ReferenceAssembliesProp = referenceAssemblies;
+            CompOptions = cSharpCompilationOptions;
         }
 
         #region Properties
@@ -28,36 +39,21 @@ namespace CodeCompilerNs
            { "WindowsRuntimeApplication", ".exe" },
         };
 
-        public static IEnumerable<string> DefaultNamespaces = new[]
-            {
-                "System",
-            };
+        private static CSharpCompilationOptions CompOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+                .WithOverflowChecks(true)
+                .WithOptimizationLevel(OptimizationLevel.Release);
 
-        private static CSharpCompilationOptions DefaultCompilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-                .WithOverflowChecks(true).WithOptimizationLevel(OptimizationLevel.Release)
-                .WithUsings(DefaultNamespaces);
+
+        private static IEnumerable<PortableExecutableReference> ReferenceAssembliesProp = ReferenceAssemblies.Net461;
 
         private static LanguageVersion LanguageVersion = LanguageVersion.Default;
-
-        private IEnumerable<MetadataReference> defaultReferences;
-        private string _runtimePath = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\{0}.dll";
-        private void PrepareDefaultReferences(string runtimePath)
-        {
-            runtimePath = !String.IsNullOrEmpty(runtimePath) ? runtimePath : _runtimePath;
-            defaultReferences = new[]
-            {
-                MetadataReference.CreateFromFile(string.Format(runtimePath, "mscorlib")),
-                MetadataReference.CreateFromFile(string.Format(runtimePath, "System")),
-                MetadataReference.CreateFromFile(string.Format(runtimePath, "System.Core"))
-            };
-        }
         #endregion
 
 
         #region Private functions
         private CSharpCompilation CreateCompilation(string fileName, SyntaxTree parsedSyntaxTree)
         {
-            return CSharpCompilation.Create(fileName, new SyntaxTree[] { parsedSyntaxTree }, defaultReferences, DefaultCompilationOptions);
+            return CSharpCompilation.Create(fileName, new SyntaxTree[] { parsedSyntaxTree }, references: ReferenceAssembliesProp, CompOptions);
         }
 
         private SyntaxTree ParseCode(string fileString, string filePath = "")
@@ -72,6 +68,14 @@ namespace CodeCompilerNs
             return compilation.Emit(outputPath);
         }
 
+        private Assembly EmitAssemblyToMemory(CSharpCompilation compilation, ref EmitResult emitResult)
+        {
+            using var stream = new MemoryStream();
+            emitResult = compilation.Emit(stream);
+            stream.Position = 0;
+            return Assembly.Load(stream.ToArray());
+        }
+
         private string ReadFileFromPath(string inputPath)
         {
             return File.ReadAllText(inputPath);
@@ -81,7 +85,7 @@ namespace CodeCompilerNs
         {
             string ext = "";
             fileName = string.IsNullOrEmpty(fileName) ? Path.GetFileNameWithoutExtension(inputPath) : fileName;
-            extensionsDct.TryGetValue(DefaultCompilationOptions.OutputKind.ToString(), out ext);
+            extensionsDct.TryGetValue(CompOptions.OutputKind.ToString(), out ext);
             outputPath = Path.Combine(outputPath, fileName + ext);
         }
         #endregion
@@ -97,9 +101,12 @@ namespace CodeCompilerNs
             return EmitAssemblyToFile(compilation, outputPath);
         }
 
-        public byte[] CreateAssemblyToMemory(string inputPath, ref EmitResult emitResult)
+        public Assembly CreateAssemblyToMemory(string inputPath, ref EmitResult emitResult)
         {
-            throw new NotImplementedException();
+            string source = ReadFileFromPath(inputPath);
+            SyntaxTree parsedSyntaxTree = ParseCode(source, "");
+            CSharpCompilation compilation = CreateCompilation("TempCompilation", parsedSyntaxTree);
+            return EmitAssemblyToMemory(compilation, ref emitResult);
         }
         #endregion
     }
